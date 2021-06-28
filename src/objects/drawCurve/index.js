969501a9db0a4fedff35de8670be321f5f83e99c
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2021-06-28 16:38:27 -0600

    Cleaned drawCurve Logic

--- src/objects/drawCurve/index.js
+++ src/objects/drawCurve/index.js
@@ -12,12 +12,8 @@ export default function drawCurve(a, b) {
   const normal = new THREE.Vector3().subVectors(a, b);
   normal.normalize();
 
-  const midStart = mid
-    .clone()
-    .add(normal.clone().multiplyScalar(distance * 0.25));
-  const midEnd = mid
-    .clone()
-    .add(normal.clone().multiplyScalar(distance * -0.25));
+  const midStart = mid.clone().add(normal.clone().multiplyScalar(distance * 0.25));
+  const midEnd = mid.clone().add(normal.clone().multiplyScalar(distance * -0.25));
 
   const splineCurveA = new THREE.CubicBezierCurve3(a, a, midStart, mid);
   const splineCurveB = new THREE.CubicBezierCurve3(mid, midEnd, b, b);
@@ -29,15 +25,17 @@ export default function drawCurve(a, b) {
 
   const lineGeometry = new THREE.BufferGeometry();
   const positions = new Float32Array(points.length * 3);
-  for (let ii = 0; ii < points.length; ii += 1) {
-    positions[ii * 3 + 0] = points[ii].x;
-    positions[ii * 3 + 1] = points[ii].y;
-    positions[ii * 3 + 2] = points[ii].z;
+  for (let i = 0; i < points.length; i += 1) {
+    positions[i * 3 + 0] = points[i].x;
+    positions[i * 3 + 1] = points[i].y;
+    positions[i * 3 + 2] = points[i].z;
   }
+
   lineGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3),
   );
+
   lineGeometry.setDrawRange(0, 0);
 
   const lineMaterial = new THREE.LineBasicMaterial({
