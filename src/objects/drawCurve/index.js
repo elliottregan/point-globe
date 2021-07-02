73089328dcd84162924497f3b5c5df0315aabcba
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2021-07-01 18:28:54 -0600

    Fixed Accidental Removal of Material Properties: linewidth/opacity

--- src/objects/drawCurve/index.js
+++ src/objects/drawCurve/index.js
@@ -64,7 +64,12 @@ export default function drawCurve(a, b) {
   geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
   geometry.setDrawRange(0, 0);
 
-  const material = new THREE.LineBasicMaterial({ transparent: true, vertexColors: true });
+  const material = new THREE.LineBasicMaterial({
+    linewidth: 2,
+    opacity: 0.75,
+    transparent: true,
+    vertexColors: true,
+  });
 
   const line = new THREE.Line(geometry, material);
   line.currentPoint = 0;
